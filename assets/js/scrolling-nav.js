78be23700eff1fb01899cbf3c914021d7d9348ff
John Douglas <nightwolfgaming@Johns-MacBook-Pro.local>
2019-03-21 16:24:49 -0400

    Test 2

--- assets/js/scrolling-nav.js
+++ assets/js/scrolling-nav.js
@@ -1,8 +1,8 @@
-(function($) {
+function startScroll() {
   "use strict"; // Start of use strict
 
   // Smooth scrolling using jQuery easing
-  $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function() {
+  $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function() { 
     if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
       var target = $(this.hash);
       target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
@@ -26,4 +26,4 @@
     offset: 56
   });
 
-})(jQuery); // End of use strict
+}; 
